4ccdd27df29e91e4b62e72b06758191823a8b35d
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2024-10-23 23:30:32 +0300

    Update test.py

--- simple_test/test.py
+++ simple_test/test.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
